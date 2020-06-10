de021bb1f8188430e5c4e40f0f9ecd790682890f
Amine Yildiz <yildizamine95@gmail.com>
2020-06-10 18:58:45 +0300

    Spring Boot Intellij IDEA

---
+++ src/main/java/com/amine/HellospringbootApplication.java
@@ -0,0 +1,15 @@
+package com.amine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HellospringbootApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/amine/WebController.java
@@ -0,0 +1,14 @@
+package com.amine;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        System.out.println("Saying hello world Spring Boot...");
+        return "templates/hello";
+    }
+}
